7fed07ba62ba0d45102783d27ad6d01595db3950
jpalavecino55 <60577660+jpalavecino55@users.noreply.github.com>
2020-11-21 17:42:34 -0500

    Update LucifinderApp.swift

--- Lucifinder/Lucifinder/LucifinderApp.swift
+++ Lucifinder/Lucifinder/LucifinderApp.swift
@@ -1,8 +1,8 @@
 //
-//  LucifinderApp.swift
-//  Lucifinder
+//  Lucifinder_v2App.swift
+//  Lucifinder_v2
 //
-//  Created by Alexandra Spector on 10/15/20.
+//  Created by Jeremy Palavecino on 11/16/20.
 //
 
 import SwiftUI
@@ -11,7 +11,13 @@ import SwiftUI
 struct LucifinderApp: App {
     var body: some Scene {
         WindowGroup {
-            AppContentView()
+            ContentView()
         }
+    }
+}
+
+struct LucifinderApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }

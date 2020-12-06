dfcde56a44460bca0b419d294dc355e20c489f9c
jpalavecino55 <60577660+jpalavecino55@users.noreply.github.com>
2020-12-06 00:29:20 -0500

    Update ImagePickerCamera.swift

--- Lucifinder/Lucifinder/ImagePickerCamera.swift
+++ Lucifinder/Lucifinder/ImagePickerCamera.swift
@@ -17,8 +17,28 @@ struct ImagePickerCamera: UIViewControllerRepresentable {
         }
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
             if let uiImage = info[.originalImage] as? UIImage {
                 parent.image = uiImage
+                
+                let today = Date()
+                let formatter = DateFormatter()
+                formatter.dateFormat = "HH:mm_E_d_MM_y"
+                let tempstring = formatter.string(from: today)
+                let fileType = ".jpg"
+                
+                let docsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                let fileName = tempstring + fileType
+                let fileURL = docsDirectory.appendingPathComponent(fileName)
+                if let data = uiImage.jpegData(compressionQuality: 1.0),!FileManager.default.fileExists(atPath: fileURL.path) {
+                    do {
+                        try data.write(to: fileURL)
+                        //print("file written")
+                        parent.url = fileURL
+                    } catch {
+                        print("error writing file:", error)
+                    }
+                }
             }
             
             parent.presentationMode.wrappedValue.dismiss()
@@ -26,6 +46,7 @@ struct ImagePickerCamera: UIViewControllerRepresentable {
     }
     @Environment(\.presentationMode) var presentationMode
     @Binding var image: UIImage?
+    @Binding var url: URL?
     
     func makeCoordinator() -> PickerCoordinator {
         PickerCoordinator(self)
